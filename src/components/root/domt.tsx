a21012e8f0ad66fe12877c05e0c02b8e8574ef8c
gravender <ss.noah@gmail.com>
2022-12-13 16:18:47 +1100

    Fixed styling and added more info for maze search

--- src/components/root/domt.tsx
+++ src/components/root/domt.tsx
@@ -33,6 +33,35 @@ const MainPage = () => {
           reducing the number of moves to evaluate.
         </p>
       </div>
+      <h1>BFS and DFS Algorithms</h1>
+      <div className="SearchIntro">
+        <p>
+          BFS and DFS algorithms are two common methods used to search through a
+          maze. Both algorithms have their own unique characteristics and can be
+          used to solve different types of problems.
+        </p>
+      </div>
+      <div className="SearchInfo">
+        <h2>BFS (Breadth-First Search) Algorithm</h2>
+        <p>
+          BFS is an algorithm that starts at the starting point of the maze and
+          explores all the neighboring nodes (adjacent cells) before moving on
+          to the next level of nodes. This means that BFS will always explore
+          the entire breadth of a level before moving on to the next level. This
+          is useful for finding the shortest path between two points in a maze.
+        </p>
+      </div>
+      <div className="SearchInfo">
+        <h2>DFS (Depth-First Search) Algorithm</h2>
+        <p>
+          DFS is an algorithm that starts at the starting point of the maze and
+          explores as far as possible along each branch before backtracking.
+          This means that DFS will explore all the possible paths in a maze
+          before choosing the best one. This is useful for solving problems that
+          require finding all possible solutions or for finding paths with a
+          specific property, such as the longest path.
+        </p>
+      </div>
     </div>
   );
 };
